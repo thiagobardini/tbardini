8478474864ea8b1da5984935bf684b5174c79199
thiagobadini <thiagobardini@hotmail.com>
2023-09-06 14:16:38 -0400

    Fix: Re-render and resize text on viewport change - Hero Page

--- src/Components/HomeText.js
+++ src/Components/HomeText.js
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
-import { Box, styled, Typography } from "@mui/material";
+import { Box, styled, Typography, useMediaQuery } from "@mui/material";
 import { TypeAnimation } from "react-type-animation";
 import { useSelector } from "react-redux";
 import TextAnimation from "./TextAnimation";
 import ButtonFab from "./ButtonFab";
 import ScrollTrigger from "react-scroll-trigger";
+import { useTheme } from "@mui/material/styles";
 
 const CustomBox = styled(Box)(({ theme }) => ({
   display: "flex",
@@ -22,13 +23,16 @@ const HomeText = () => {
   const [key, setKey] = useState(0);
   const darkMode = useSelector((state) => state.theme.darkMode);
 
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.up("sm"));
+
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
   useEffect(() => {
     setKey((prevKey) => prevKey + 1);
-  }, [darkMode]);
+  }, [darkMode, matches]);
 
   const onEnterViewport = (animationClass) => {
     const element = document.getElementById(animationClass);
@@ -45,16 +49,10 @@ const HomeText = () => {
           textShadow:
             "-1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000",
           color: darkMode ? "#eeeeee" : "#eeeeee",
-          // fontWeight: 800,
           position: "relative",
           borderRadius: "5px",
           fontSize: "3rem",
-          // fontFamily: "TuskerGrotesk",
-          // fontFamily: "PPNeueMontreal-Medium",
-          // fontFamily: "YatraOne-Regular",
-          // fontFamily: "GothamSSm-Bold",
           fontFamily: "GothamSSm-Bold",
-          // letterSpacing: "0.1em",
         }}
       >
         <TypeAnimation
@@ -68,54 +66,93 @@ const HomeText = () => {
       <Box sx={{ py: 3 }}>
         <TextAnimation darkMode={true} />
       </Box>
-
       <Box
         sx={{
           px: 1,
         }}
       >
         <ScrollTrigger onEnter={() => onEnterViewport("fade-slide-up-h1")}>
-          <Typography
-            variant="h6"
-            id="fade-slide-up-h1"
-            sx={{
-              color: (theme) => theme.palette.text.secondary,
-              backdropFilter: darkMode ? "blur(2px)" : "blur(3px)",
-              textShadow: darkMode
-                ? "none"
-                : {
-                    xs: "0 0 5px #d6d3d1, 0 0 5px #d6d3d1, 0 0 5px #d6d3d1, 0 0 5px #d6d3d1, 0 0 5px #d6d3d1, 0 0 5px #d6d3d1",
-                    md: "none",
-                  },
-              fontWeight: 600,
-              opacity: 0,
-              transform: "translateY(20px)",
-              transition: "opacity 1s ease-in-out, transform 1s ease-in-out",
-              fontFamily: "GothamSSm-Light",
-              "&.fade-slide-up-h1": {
-                opacity: 1,
-                transform: "translateY(0)",
-              },
-            }}
-          >
-            I'm a Boston based{" "}
-            <span
-              style={{
-                fontFamily: "GothamSSm-Bold",
-                color: darkMode ? "#eeeeee" : "#222831",
+          {matches ? (
+            <Typography
+              variant="h6"
+              id="fade-slide-up-h1"
+              sx={{
+                color: (theme) => theme.palette.text.secondary,
+                backdropFilter: darkMode ? "blur(2px)" : "blur(3px)",
                 textShadow: darkMode
                   ? "none"
-                  : "0 0 5px #ffffff, 0 0 5px #ffffff, 0 0 5px #ffffff, 0 0 5px #ffffff, 0 0 5px #ffffff, 0 0 5px #ffffff",
+                  : {
+                      xs: "0 0 5px #d6d3d1, 0 0 5px #d6d3d1, 0 0 5px #d6d3d1, 0 0 5px #d6d3d1, 0 0 5px #d6d3d1, 0 0 5px #d6d3d1",
+                      md: "none",
+                    },
+                fontWeight: 600,
+                opacity: 0,
+                transform: "translateY(20px)",
+                transition: "opacity 1s ease-in-out, transform 1s ease-in-out",
+                fontFamily: "GothamSSm-Light",
+                "&.fade-slide-up-h1": {
+                  opacity: 1,
+                  transform: "translateY(0)",
+                },
               }}
             >
-              software engineer
-            </span>{" "}
-            turned problem-solver, bringing creativity and design thinking to
-            every line of code.
-          </Typography>
+              I'm a Boston based{" "}
+              <span
+                style={{
+                  fontFamily: "GothamSSm-Bold",
+                  color: darkMode ? "#eeeeee" : "#222831",
+                  textShadow: darkMode
+                    ? "none"
+                    : "0 0 5px #ffffff, 0 0 5px #ffffff, 0 0 5px #ffffff, 0 0 5px #ffffff, 0 0 5px #ffffff, 0 0 5px #ffffff",
+                }}
+              >
+                software engineer
+              </span>{" "}
+              turned problem-solver, bringing creativity and design thinking to
+              every line of code.
+            </Typography>
+          ) : (
+            <Typography
+              variant="body1"
+              id="fade-slide-up-h1"
+              sx={{
+                color: (theme) => theme.palette.text.secondary,
+                backdropFilter: darkMode ? "blur(2px)" : "blur(3px)",
+                textShadow: darkMode
+                  ? "none"
+                  : {
+                      xs: "0 0 5px #d6d3d1, 0 0 5px #d6d3d1, 0 0 5px #d6d3d1, 0 0 5px #d6d3d1, 0 0 5px #d6d3d1, 0 0 5px #d6d3d1",
+                      md: "none",
+                    },
+                fontWeight: 600,
+                opacity: 0,
+                transform: "translateY(20px)",
+                transition: "opacity 1s ease-in-out, transform 1s ease-in-out",
+                fontFamily: "GothamSSm-Light",
+                "&.fade-slide-up-h1": {
+                  opacity: 1,
+                  transform: "translateY(0)",
+                },
+              }}
+            >
+              I'm a Boston based{" "}
+              <span
+                style={{
+                  fontFamily: "GothamSSm-Bold",
+                  color: darkMode ? "#eeeeee" : "#222831",
+                  textShadow: darkMode
+                    ? "none"
+                    : "0 0 5px #ffffff, 0 0 5px #ffffff, 0 0 5px #ffffff, 0 0 5px #ffffff, 0 0 5px #ffffff, 0 0 5px #ffffff",
+                }}
+              >
+                software engineer
+              </span>{" "}
+              turned problem-solver, bringing creativity and design thinking to
+              every line of code.
+            </Typography>
+          )}
         </ScrollTrigger>
       </Box>
-
       <Box mt={4} sx={{ width: "auto" }}>
         <Box sx={{ "& > :not(style)": { m: 1 } }}>
           <ButtonFab
